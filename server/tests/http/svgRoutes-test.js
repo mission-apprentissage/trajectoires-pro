47d1a46eba824424673fcde0d70aa41409addfa3
Eric Plaquevent <eric.plaquevent@gmail.com>
2022-05-03 16:50:22 +0200

    feat: use v1 of svg from Figma and insert font

--- server/tests/http/svgRoutes-test.js
+++ server/tests/http/svgRoutes-test.js
@@ -5,10 +5,10 @@ describe("svgRoutes", () => {
   it("Vérifie que la route fonctionne", async () => {
     const { httpClient } = await startServer();
 
-    const response = await httpClient.get("/api/svg/badge");
+    const response = await httpClient.get("/api/svg/etablissement/123");
 
     assert.strictEqual(response.status, 200);
     assert.ok(response.headers["content-type"].includes("image/svg+xml"));
-    assert.ok(response.data.includes("taux d'insertion"));
+    assert.ok(response.data.includes("sont en emploi 6 mois"));
   });
 });
