6f38e53f9ff209d705413f1e437ed1e0cf8fd1be
Eric Plaquevent <eric.plaquevent@gmail.com>
2022-05-03 18:08:41 +0200

    feat: plug SVG to data

--- server/tests/http/svgRoutes-test.js
+++ server/tests/http/svgRoutes-test.js
@@ -1,14 +1,64 @@
 const assert = require("assert");
 const { startServer } = require("../utils/testUtils");
+const { dbCollection } = require("../../src/common/mongodb");
 
 describe("svgRoutes", () => {
-  it("Vérifie que la route fonctionne", async () => {
-    const { httpClient } = await startServer();
+  beforeEach(async () => {
+    await dbCollection("insertJeunes").insertOne({
+      millesime: "2022-2021",
+      code_formation: "1022105",
+      type: "apprentissage",
+      diplome_renove_ou_nouveau: "non",
+      duree_de_formation: 1,
+      libelle_de_etablissement: "Centre de Formation",
+      libelle_de_la_formation: "cuisinier en desserts de restaurant",
+      region: {
+        code: "84",
+        nom: "Auvergne-Rhône-Alpes",
+      },
+      taux_de_poursuite_etudes: 42,
+      taux_emploi_12_mois_apres_la_sortie: 58,
+      taux_emploi_6_mois_apres_la_sortie: 50,
+      type_de_diplome: "MC5",
+      uai_de_etablissement: "0751234J",
+    });
+  });
 
-    const response = await httpClient.get("/api/svg/etablissement/123");
+  it("should return a svg file if data exist", async () => {
+    const { httpClient } = await startServer();
+    const response = await httpClient.get("/api/svg/etablissement/0751234J/1022105/2022-2021");
 
     assert.strictEqual(response.status, 200);
     assert.ok(response.headers["content-type"].includes("image/svg+xml"));
+    assert.ok(response.data.includes("50%"));
     assert.ok(response.data.includes("sont en emploi 6 mois"));
+  });
+
+  it("should get the horizontal file if asked", async () => {
+    const { httpClient } = await startServer();
+    const response = await httpClient.get("/api/svg/etablissement/0751234J/1022105/2022-2021?direction=horizontal");
+
+    assert.strictEqual(response.status, 200);
+    assert.ok(response.headers["content-type"].includes("image/svg+xml"));
+    assert.ok(response.data.includes(`width="700"`));
+    assert.ok(response.data.includes("50%"));
+    assert.ok(response.data.includes("sont en emploi 6 mois"));
+  });
+
+  it("should get the vertical file if asked direction is not list", async () => {
+    const { httpClient } = await startServer();
+    const response = await httpClient.get("/api/svg/etablissement/0751234J/1022105/2022-2021?direction=diagonal");
+
+    assert.strictEqual(response.status, 200);
+    assert.ok(response.headers["content-type"].includes("image/svg+xml"));
+    assert.ok(response.data.includes(`width="320"`));
+    assert.ok(response.data.includes("50%"));
+    assert.ok(response.data.includes("sont en emploi 6 mois"));
+  });
+
+  it("should return a 404 if data does not exist", async () => {
+    const { httpClient } = await startServer();
+    const response = await httpClient.get("/api/svg/etablissement/0751234P/1022101/2022-2021");
+    assert.strictEqual(response.status, 404);
   });
 });
