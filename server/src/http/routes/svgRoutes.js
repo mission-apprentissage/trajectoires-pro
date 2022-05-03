e10ec0dcdc4b31720effcbd6c5d12164f38493cc
Eric Plaquevent <eric.plaquevent@gmail.com>
2022-05-03 17:22:41 +0200

    feat: use v1 of svg from Figma and insert font (#4)

--- server/src/http/routes/svgRoutes.js
+++ server/src/http/routes/svgRoutes.js
@@ -1,24 +1,41 @@
+const fs = require("fs");
 const express = require("express");
 const logger = require("../../common/logger");
 const tryCatch = require("../middlewares/tryCatchMiddleware");
-
 const ejs = require("ejs");
 const path = require("path");
 
-const insertionBadgeTemplate = path.join(__dirname, `../templates/insertionBadge.svg.ejs`);
+const establishmentSvgTemplate = path.join(__dirname, `../templates/establishment.svg.ejs`);
 
 /**
- * Sample route module for displaying hello message
+ * load base64 font, so that it can be injected in svg
+ */
+let base64Font = null;
+const loadBase64Font = () => {
+  if (!base64Font) {
+    const buffer = fs.readFileSync(path.join(__dirname, `../templates/fonts/Marianne-Regular.woff`));
+    base64Font = buffer.toString("base64");
+  }
+
+  return base64Font;
+};
+
+/**
+ * Route module to render SVG images
  */
 module.exports = () => {
   const router = express.Router();
 
   router.get(
-    "/api/svg/badge",
+    "/api/svg/etablissement/:uai",
     tryCatch(async (req, res) => {
-      const rate = 87;
-      const color = "#4c1";
-      const svg = await ejs.renderFile(insertionBadgeTemplate, { rate, color }, { async: true });
+      // TODO find data in db with uai
+      const { uai } = req.params;
+      logger.info("uai", uai);
+
+      const base64Font = loadBase64Font();
+      const data = { base64Font };
+      const svg = await ejs.renderFile(establishmentSvgTemplate, data, { async: true });
 
       res.setHeader("content-type", "image/svg+xml");
       return res.status(200).send(svg);
