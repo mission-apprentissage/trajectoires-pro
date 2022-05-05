2c6df4b2099bb77af27c9fceb9fd6246a972ef42
Eric Plaquevent <eric.plaquevent@gmail.com>
2022-05-05 12:35:46 +0200

    feat: insert national data into db

--- server/src/cli.js
+++ server/src/cli.js
@@ -3,12 +3,25 @@ const { program: cli } = require("commander");
 const runScript = require("./common/runScript");
 const migrate = require("./jobs/migrate");
 const importInsertJeunes = require("./jobs/importInsertJeunes");
+const importNationalData = require("./jobs/importNationalData");
 const { createReadStream } = require("fs");
 
 cli
+  .command("importNationalData")
+  .description("Importe les données InserJeunes nationales en base")
+  .argument("[file]", "Un fichier CSV contenant les données InserJeunes nationales", createReadStream)
+  .option("--millesime", "Le millesime du fichier (obligatoire si un fichier est passé en paramètre)")
+  .option("--type", "Le type de données: 'apprentissage' ou 'pro' (obligatoire si un fichier est passé en paramètre)")
+  .action((file, options = {}) => {
+    runScript(() => {
+      return importNationalData({ input: file, ...options });
+    });
+  });
+
+cli
   .command("importInsertJeunes")
-  .description("Importe les fichiers InsertJeunes en base")
-  .argument("[file]", "Un fichier CSV contenant les informations InsertJeunes", createReadStream)
+  .description("Importe les fichiers InserJeunes des établissements en base")
+  .argument("[file]", "Un fichier CSV contenant les données InserJeunes des établissements", createReadStream)
   .option("--millesime", "Le millesime du fichier (obligatoire si un fichier est passé en paramètre)")
   .action((file, options = {}) => {
     runScript(() => {
---
+++ server/src/common/collections/inserJeunesNational.js
@@ -0,0 +1,40 @@
+const { object, objectId, string, integer } = require("./schemas/jsonSchemaTypes");
+
+module.exports = {
+  name: "inserJeunesNational",
+  schema: () => {
+    return object(
+      {
+        _id: objectId(),
+        millesime: string(),
+        type: string({ enum: ["apprentissage", "pro"] }),
+        code_formation: string(),
+        type_de_diplome: string(),
+        libelle_de_la_formation: string(),
+        duree_de_formation: integer(),
+        diplome_renove_ou_nouveau: string(),
+        taux_de_poursuite_etudes: integer(),
+        nb_en_poursuite_etudes: integer(),
+        nb_en_annee_terminale: integer(),
+        taux_emploi_6_mois_apres_la_sortie: integer(),
+        nb_en_emploi_6_mois_apres_la_sortie: integer(),
+        nb_sortants_6_mois_apres_la_sortie: integer(),
+        taux_emploi_12_mois_apres_la_sortie: integer(),
+        nb_en_emploi_12_mois_apres_la_sortie: integer(),
+      },
+      {
+        required: [
+          "millesime",
+          "type",
+          "code_formation",
+          "type_de_diplome",
+          "libelle_de_la_formation",
+          "diplome_renove_ou_nouveau",
+        ],
+      }
+    );
+  },
+  indexes: () => {
+    return [[{ millesime: 1, code_formation: 1 }, { unique: true }]];
+  },
+};
