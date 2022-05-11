b0f98252856c66e035096c80247ce11dd5b00295
Eric Plaquevent <eric.plaquevent@gmail.com>
2022-05-11 14:35:35 +0200

    feat: widget for national data
    niveau certification

--- server/src/http/routes/svgRoutes.js
+++ server/src/http/routes/svgRoutes.js
@@ -8,9 +8,17 @@ const Joi = require("joi");
 const { dbCollection } = require("../../common/mongodb");
 const { validate } = require("../utils/validators");
 
-const establishmentSvgTemplates = {
-  vertical: path.join(__dirname, `../templates/establishment-vertical.svg.ejs`),
-  horizontal: path.join(__dirname, `../templates/establishment-horizontal.svg.ejs`),
+const svgTemplates = {
+  // local : action de formation (formation donnée dans un établissement donné)
+  formation: {
+    vertical: path.join(__dirname, `../templates/formation-vertical.svg.ejs`),
+    horizontal: path.join(__dirname, `../templates/formation-horizontal.svg.ejs`),
+  },
+  // national : diplome
+  certification: {
+    vertical: path.join(__dirname, `../templates/certification-vertical.svg.ejs`),
+    horizontal: path.join(__dirname, `../templates/certification-horizontal.svg.ejs`),
+  },
 };
 
 /**
@@ -34,11 +42,11 @@ const labels = /** @type {const} */ ({
 /**
  * Create on array of rates to feed the ejs template
  *
- * @param {{taux_emploi_6_mois_apres_la_sortie?: number, taux_de_poursuite_etudes?: number}} inserJeunesEtablissementsData
+ * @param {{taux_emploi_6_mois_apres_la_sortie?: number, taux_de_poursuite_etudes?: number}} inserJeunesData
  * @returns {Array<{rate: number | undefined, labels: string[]}>}
  */
-const getRates = (inserJeunesEtablissementsData) => {
-  const { taux_emploi_6_mois_apres_la_sortie, taux_de_poursuite_etudes } = inserJeunesEtablissementsData;
+const getRates = (inserJeunesData) => {
+  const { taux_emploi_6_mois_apres_la_sortie, taux_de_poursuite_etudes } = inserJeunesData;
   const rates = Object.entries({ taux_emploi_6_mois_apres_la_sortie, taux_de_poursuite_etudes })
     .filter(([, value]) => !!value || value === 0)
     .map(([key, value]) => {
@@ -73,7 +81,7 @@ module.exports = () => {
 
       const { direction = "vertical" } = query;
 
-      const inserJeunesEtablissementsData = await dbCollection("inserJeunesEtablissements").findOne(
+      const inserJeunesData = await dbCollection("inserJeunesEtablissements").findOne(
         {
           uai_de_etablissement,
           code_formation,
@@ -84,11 +92,54 @@ module.exports = () => {
         }
       );
 
-      if (!inserJeunesEtablissementsData) {
+      if (!inserJeunesData) {
         return res.status(404).send("UAI, code formation et/ou millésime invalide");
       }
 
-      const rates = getRates(inserJeunesEtablissementsData);
+      const rates = getRates(inserJeunesData);
+      if (rates.length === 0) {
+        return res.status(404).send("Donnée non disponible");
+      }
+
+      const base64Font = loadBase64Font();
+      const data = { base64Font, rates };
+      const svg = await ejs.renderFile(svgTemplates.formation[direction] ?? svgTemplates.formation.vertical, data, {
+        async: true,
+      });
+
+      res.setHeader("content-type", "image/svg+xml");
+      return res.status(200).send(svg);
+    })
+  );
+
+  /**
+   * e.g: GET /api/svg/code_formation/32221023012/millesime/2020-2019?direction=horizontal
+   */
+  router.get(
+    "/api/svg/code_formation/:code_formation/millesime/:millesime",
+    tryCatch(async ({ params, query }, res) => {
+      const { code_formation, millesime } = await validate(params, {
+        code_formation: Joi.string().required(),
+        millesime: Joi.string().required(),
+      });
+
+      const { direction = "vertical" } = query;
+
+      const inserJeunesData = await dbCollection("inserJeunesNationals").findOne(
+        {
+          code_formation,
+          millesime,
+        },
+        {
+          projection: { taux_emploi_6_mois_apres_la_sortie: 1, taux_de_poursuite_etudes: 1 },
+        }
+      );
+
+      if (!inserJeunesData) {
+        return res.status(404).send("Code formation et/ou millésime invalide");
+      }
+
+      const rates = getRates(inserJeunesData);
       if (rates.length === 0) {
         return res.status(404).send("Donnée non disponible");
       }
@@ -96,7 +147,7 @@ module.exports = () => {
       const base64Font = loadBase64Font();
       const data = { base64Font, rates };
       const svg = await ejs.renderFile(
-        establishmentSvgTemplates[direction] ?? establishmentSvgTemplates.vertical,
+        svgTemplates.certification[direction] ?? svgTemplates.certification.vertical,
         data,
         { async: true }
       );
--- server/tests/http/svgRoutes-test.js
+++ server/tests/http/svgRoutes-test.js
@@ -4,176 +4,267 @@ const { dbCollection } = require("../../src/common/mongodb");
 const { inserInserJeunesEtablissements } = require("../utils/fakeData");
 
 describe("svgRoutes", () => {
-  beforeEach(async () => {
-    await inserInserJeunesEtablissements({
-      taux_emploi_6_mois_apres_la_sortie: 50,
+  describe("formation", () => {
+    beforeEach(async () => {
+      await inserInserJeunesEtablissements({
+        taux_emploi_6_mois_apres_la_sortie: 50,
+      });
+    });
+
+    it("should return a svg file if data exist", async () => {
+      const { httpClient } = await startServer();
+      const response = await httpClient.get("/api/svg/uai/0751234J/code_formation/1022105/millesime/2022-2021");
+
+      assert.strictEqual(response.status, 200);
+      assert.ok(response.headers["content-type"].includes("image/svg+xml"));
+      assert.ok(response.data.includes("50%"));
+      assert.ok(response.data.includes("sont en emploi 6 mois"));
+      assert.ok(response.data.includes("poursuivent leurs études"));
+    });
+
+    it("should get the horizontal file if asked", async () => {
+      const { httpClient } = await startServer();
+      const response = await httpClient.get(
+        "/api/svg/uai/0751234J/code_formation/1022105/millesime/2022-2021?direction=horizontal"
+      );
+
+      assert.strictEqual(response.status, 200);
+      assert.ok(response.headers["content-type"].includes("image/svg+xml"));
+      assert.ok(response.data.includes(`width="700"`));
+      assert.ok(response.data.includes("50%"));
+      assert.ok(response.data.includes("sont en emploi 6 mois"));
+      assert.ok(response.data.includes("poursuivent leurs études"));
+    });
+
+    it("should get the vertical file if asked direction is not list", async () => {
+      const { httpClient } = await startServer();
+      const response = await httpClient.get(
+        "/api/svg/uai/0751234J/code_formation/1022105/millesime/2022-2021?direction=diagonal"
+      );
+
+      assert.strictEqual(response.status, 200);
+      assert.ok(response.headers["content-type"].includes("image/svg+xml"));
+      assert.ok(response.data.includes(`width="320"`));
+      assert.ok(response.data.includes(`height="323"`));
+      assert.ok(response.data.includes("50%"));
+      assert.ok(response.data.includes("sont en emploi 6 mois"));
+      assert.ok(response.data.includes("poursuivent leurs études"));
+    });
+
+    it("should display only one data if present - vertical", async () => {
+      await dbCollection("inserJeunesEtablissements").insertOne({
+        millesime: "2022-2021",
+        code_formation: "1022105",
+        type: "apprentissage",
+        diplome_renove_ou_nouveau: "non",
+        duree_de_formation: 1,
+        libelle_de_etablissement: "Centre de Formation",
+        libelle_de_la_formation: "cuisinier en desserts de restaurant",
+        region: {
+          code: "84",
+          nom: "Auvergne-Rhône-Alpes",
+        },
+        taux_emploi_12_mois_apres_la_sortie: 58,
+        taux_emploi_6_mois_apres_la_sortie: 50,
+        type_de_diplome: "MC5",
+        uai_de_etablissement: "0751234K",
+      });
+      const { httpClient } = await startServer();
+      const response = await httpClient.get("/api/svg/uai/0751234K/code_formation/1022105/millesime/2022-2021");
+
+      assert.strictEqual(response.status, 200);
+      assert.ok(response.headers["content-type"].includes("image/svg+xml"));
+      assert.ok(response.data.includes(`width="320"`));
+      assert.ok(response.data.includes(`height="268"`));
+      assert.ok(response.data.includes("50%"));
+      assert.ok(response.data.includes("sont en emploi 6 mois"));
+      assert.ok(!response.data.includes("poursuivent leurs études"));
+    });
+
+    it("should display only one data if present - horizontal", async () => {
+      await dbCollection("inserJeunesEtablissements").insertOne({
+        millesime: "2022-2021",
+        code_formation: "1022105",
+        type: "apprentissage",
+        diplome_renove_ou_nouveau: "non",
+        duree_de_formation: 1,
+        libelle_de_etablissement: "Centre de Formation",
+        libelle_de_la_formation: "cuisinier en desserts de restaurant",
+        region: {
+          code: "84",
+          nom: "Auvergne-Rhône-Alpes",
+        },
+        taux_emploi_12_mois_apres_la_sortie: 58,
+        taux_emploi_6_mois_apres_la_sortie: 50,
+        type_de_diplome: "MC5",
+        uai_de_etablissement: "0751234K",
+      });
+      const { httpClient } = await startServer();
+      const response = await httpClient.get(
+        "/api/svg/uai/0751234K/code_formation/1022105/millesime/2022-2021?direction=horizontal"
+      );
+
+      assert.strictEqual(response.status, 200);
+      assert.ok(response.headers["content-type"].includes("image/svg+xml"));
+      assert.ok(response.data.includes(`width="585"`));
+      assert.ok(response.data.includes("50%"));
+      assert.ok(response.data.includes("sont en emploi 6 mois"));
+      assert.ok(!response.data.includes("poursuivent leurs études"));
+    });
+
+    it("should display data if value is 0", async () => {
+      await dbCollection("inserJeunesEtablissements").insertOne({
+        millesime: "2022-2021",
+        code_formation: "1022105",
+        type: "apprentissage",
+        diplome_renove_ou_nouveau: "non",
+        duree_de_formation: 1,
+        libelle_de_etablissement: "Centre de Formation",
+        libelle_de_la_formation: "cuisinier en desserts de restaurant",
+        region: {
+          code: "84",
+          nom: "Auvergne-Rhône-Alpes",
+        },
+        taux_de_poursuite_etudes: 0,
+        taux_emploi_12_mois_apres_la_sortie: 58,
+        taux_emploi_6_mois_apres_la_sortie: 50,
+        type_de_diplome: "MC5",
+        uai_de_etablissement: "0751234K",
+      });
+      const { httpClient } = await startServer();
+      const response = await httpClient.get("/api/svg/uai/0751234K/code_formation/1022105/millesime/2022-2021");
+
+      assert.strictEqual(response.status, 200);
+      assert.ok(response.headers["content-type"].includes("image/svg+xml"));
+      assert.ok(response.data.includes(`width="320"`));
+      assert.ok(response.data.includes(`height="323"`));
+      assert.ok(response.data.includes("50%"));
+      assert.ok(response.data.includes("sont en emploi 6 mois"));
+      assert.ok(response.data.includes("poursuivent leurs études"));
+    });
+
+    it("should return a 404 if data does not exist", async () => {
+      const { httpClient } = await startServer();
+      const response = await httpClient.get("/api/svg/uai/0751234P/code_formation/1022101/millesime/2022-2021");
+      assert.strictEqual(response.status, 404);
+      assert.strictEqual(response.data, "UAI, code formation et/ou millésime invalide");
+    });
+
+    it("should return a 404 if data exists but rates are empty", async () => {
+      await dbCollection("inserJeunesEtablissements").insertOne({
+        millesime: "2022-2021",
+        code_formation: "1022106",
+        type: "apprentissage",
+        diplome_renove_ou_nouveau: "non",
+        duree_de_formation: 1,
+        libelle_de_etablissement: "Centre de Formation",
+        libelle_de_la_formation: "cuisinier en desserts de restaurant",
+        region: {
+          code: "84",
+          nom: "Auvergne-Rhône-Alpes",
+        },
+        type_de_diplome: "MC5",
+        uai_de_etablissement: "0751234K",
+      });
+      const { httpClient } = await startServer();
+      const response = await httpClient.get("/api/svg/uai/0751234K/code_formation/1022106/millesime/2022-2021");
+      assert.strictEqual(response.status, 404);
+      assert.strictEqual(response.data, "Donnée non disponible");
+    });
+
+    it("should return a 400 if uai is not well formated", async () => {
+      const { httpClient } = await startServer();
+      const response = await httpClient.get("/api/svg/uai/0010001/code_formation/23220023440/millesime/2020-2019");
+      assert.strictEqual(response.status, 400);
     });
   });
 
-  it("should return a svg file if data exist", async () => {
-    const { httpClient } = await startServer();
-    const response = await httpClient.get("/api/svg/uai/0751234J/code_formation/1022105/millesime/2022-2021");
+  describe("certification", () => {
+    it("should display svg for certification", async () => {
+      await dbCollection("inserJeunesNationals").insertOne({
+        code_formation: "23830024203",
+        millesime: "2020-2019",
+        libelle_de_la_formation: "metiers de la mode - vêtement",
+        nb_en_annee_terminale: 2845,
+        nb_en_emploi_12_mois_apres_la_sortie: 427,
+        nb_en_emploi_6_mois_apres_la_sortie: 260,
+        nb_en_poursuite_etudes: 1567,
+        nb_sortants_6_mois_apres_la_sortie: 1278,
+        taux_de_poursuite_etudes: 55,
+        taux_emploi_12_mois_apres_la_sortie: 33,
+        taux_emploi_6_mois_apres_la_sortie: 20,
+        type: "pro",
+        type_de_diplome: "BAC PRO",
+        diplome_renove_ou_nouveau: "non",
+      });
+      const { httpClient } = await startServer();
+      const response = await httpClient.get("/api/svg/code_formation/23830024203/millesime/2020-2019");
 
-    assert.strictEqual(response.status, 200);
-    assert.ok(response.headers["content-type"].includes("image/svg+xml"));
-    assert.ok(response.data.includes("50%"));
-    assert.ok(response.data.includes("sont en emploi 6 mois"));
-    assert.ok(response.data.includes("poursuivent leurs études"));
-  });
-
-  it("should get the horizontal file if asked", async () => {
-    const { httpClient } = await startServer();
-    const response = await httpClient.get(
-      "/api/svg/uai/0751234J/code_formation/1022105/millesime/2022-2021?direction=horizontal"
-    );
-
-    assert.strictEqual(response.status, 200);
-    assert.ok(response.headers["content-type"].includes("image/svg+xml"));
-    assert.ok(response.data.includes(`width="700"`));
-    assert.ok(response.data.includes("50%"));
-    assert.ok(response.data.includes("sont en emploi 6 mois"));
-    assert.ok(response.data.includes("poursuivent leurs études"));
-  });
-
-  it("should get the vertical file if asked direction is not list", async () => {
-    const { httpClient } = await startServer();
-    const response = await httpClient.get(
-      "/api/svg/uai/0751234J/code_formation/1022105/millesime/2022-2021?direction=diagonal"
-    );
-
-    assert.strictEqual(response.status, 200);
-    assert.ok(response.headers["content-type"].includes("image/svg+xml"));
-    assert.ok(response.data.includes(`width="320"`));
-    assert.ok(response.data.includes(`height="323"`));
-    assert.ok(response.data.includes("50%"));
-    assert.ok(response.data.includes("sont en emploi 6 mois"));
-    assert.ok(response.data.includes("poursuivent leurs études"));
-  });
-
-  it("should display only one data if present - vertical", async () => {
-    await dbCollection("inserJeunesEtablissements").insertOne({
-      millesime: "2022-2021",
-      code_formation: "1022105",
-      type: "apprentissage",
-      diplome_renove_ou_nouveau: "non",
-      duree_de_formation: 1,
-      libelle_de_etablissement: "Centre de Formation",
-      libelle_de_la_formation: "cuisinier en desserts de restaurant",
-      region: {
-        code: "84",
-        nom: "Auvergne-Rhône-Alpes",
-      },
-      taux_emploi_12_mois_apres_la_sortie: 58,
-      taux_emploi_6_mois_apres_la_sortie: 50,
-      type_de_diplome: "MC5",
-      uai_de_etablissement: "0751234K",
+      assert.strictEqual(response.status, 200);
+      assert.ok(response.headers["content-type"].includes("image/svg+xml"));
+      assert.ok(response.data.includes("20%"));
+      assert.ok(response.data.includes("sont en emploi 6 mois"));
+      assert.ok(response.data.includes("poursuivent leurs études"));
     });
-    const { httpClient } = await startServer();
-    const response = await httpClient.get("/api/svg/uai/0751234K/code_formation/1022105/millesime/2022-2021");
 
-    assert.strictEqual(response.status, 200);
-    assert.ok(response.headers["content-type"].includes("image/svg+xml"));
-    assert.ok(response.data.includes(`width="320"`));
-    assert.ok(response.data.includes(`height="268"`));
-    assert.ok(response.data.includes("50%"));
-    assert.ok(response.data.includes("sont en emploi 6 mois"));
-    assert.ok(!response.data.includes("poursuivent leurs études"));
-  });
-
-  it("should display only one data if present - horizontal", async () => {
-    await dbCollection("inserJeunesEtablissements").insertOne({
-      millesime: "2022-2021",
-      code_formation: "1022105",
-      type: "apprentissage",
-      diplome_renove_ou_nouveau: "non",
-      duree_de_formation: 1,
-      libelle_de_etablissement: "Centre de Formation",
-      libelle_de_la_formation: "cuisinier en desserts de restaurant",
-      region: {
-        code: "84",
-        nom: "Auvergne-Rhône-Alpes",
-      },
-      taux_emploi_12_mois_apres_la_sortie: 58,
-      taux_emploi_6_mois_apres_la_sortie: 50,
-      type_de_diplome: "MC5",
-      uai_de_etablissement: "0751234K",
+    it("should return a 404 if data does not exist", async () => {
+      const { httpClient } = await startServer();
+      const response = await httpClient.get("/api/svg/code_formation/23830024203/millesime/2022-2021");
+      assert.strictEqual(response.status, 404);
+      assert.strictEqual(response.data, "Code formation et/ou millésime invalide");
     });
-    const { httpClient } = await startServer();
-    const response = await httpClient.get(
-      "/api/svg/uai/0751234K/code_formation/1022105/millesime/2022-2021?direction=horizontal"
-    );
 
-    assert.strictEqual(response.status, 200);
-    assert.ok(response.headers["content-type"].includes("image/svg+xml"));
-    assert.ok(response.data.includes(`width="585"`));
-    assert.ok(response.data.includes("50%"));
-    assert.ok(response.data.includes("sont en emploi 6 mois"));
-    assert.ok(!response.data.includes("poursuivent leurs études"));
-  });
+    it("should return a 404 if data exists but rates are empty", async () => {
+      await dbCollection("inserJeunesNationals").insertOne({
+        code_formation: "23830024203",
+        millesime: "2020-2019",
+        libelle_de_la_formation: "metiers de la mode - vêtement",
+        nb_en_annee_terminale: 2845,
+        nb_en_emploi_12_mois_apres_la_sortie: 427,
+        nb_en_emploi_6_mois_apres_la_sortie: 260,
+        nb_en_poursuite_etudes: 1567,
+        nb_sortants_6_mois_apres_la_sortie: 1278,
+        taux_emploi_12_mois_apres_la_sortie: 33,
+        type: "pro",
+        type_de_diplome: "BAC PRO",
+        diplome_renove_ou_nouveau: "non",
+      });
 
-  it("should display data if value is 0", async () => {
-    await dbCollection("inserJeunesEtablissements").insertOne({
-      millesime: "2022-2021",
-      code_formation: "1022105",
-      type: "apprentissage",
-      diplome_renove_ou_nouveau: "non",
-      duree_de_formation: 1,
-      libelle_de_etablissement: "Centre de Formation",
-      libelle_de_la_formation: "cuisinier en desserts de restaurant",
-      region: {
-        code: "84",
-        nom: "Auvergne-Rhône-Alpes",
-      },
-      taux_de_poursuite_etudes: 0,
-      taux_emploi_12_mois_apres_la_sortie: 58,
-      taux_emploi_6_mois_apres_la_sortie: 50,
-      type_de_diplome: "MC5",
-      uai_de_etablissement: "0751234K",
+      const { httpClient } = await startServer();
+      const response = await httpClient.get("/api/svg/code_formation/23830024203/millesime/2020-2019");
+      assert.strictEqual(response.status, 404);
+      assert.strictEqual(response.data, "Donnée non disponible");
     });
-    const { httpClient } = await startServer();
-    const response = await httpClient.get("/api/svg/uai/0751234K/code_formation/1022105/millesime/2022-2021");
 
-    assert.strictEqual(response.status, 200);
-    assert.ok(response.headers["content-type"].includes("image/svg+xml"));
-    assert.ok(response.data.includes(`width="320"`));
-    assert.ok(response.data.includes(`height="323"`));
-    assert.ok(response.data.includes("50%"));
-    assert.ok(response.data.includes("sont en emploi 6 mois"));
-    assert.ok(response.data.includes("poursuivent leurs études"));
-  });
+    it("should get the vertical file if asked direction is not list", async () => {
+      await dbCollection("inserJeunesNationals").insertOne({
+        code_formation: "23830024203",
+        millesime: "2020-2019",
+        libelle_de_la_formation: "metiers de la mode - vêtement",
+        nb_en_annee_terminale: 2845,
+        nb_en_emploi_12_mois_apres_la_sortie: 427,
+        nb_en_emploi_6_mois_apres_la_sortie: 260,
+        nb_en_poursuite_etudes: 1567,
+        nb_sortants_6_mois_apres_la_sortie: 1278,
+        taux_de_poursuite_etudes: 55,
+        taux_emploi_12_mois_apres_la_sortie: 33,
+        taux_emploi_6_mois_apres_la_sortie: 20,
+        type: "pro",
+        type_de_diplome: "BAC PRO",
+        diplome_renove_ou_nouveau: "non",
+      });
+      const { httpClient } = await startServer();
+      const response = await httpClient.get(
+        "/api/svg/code_formation/23830024203/millesime/2020-2019?direction=diagonal"
+      );
 
-  it("should return a 404 if data does not exist", async () => {
-    const { httpClient } = await startServer();
-    const response = await httpClient.get("/api/svg/uai/0751234P/code_formation/1022101/millesime/2022-2021");
-    assert.strictEqual(response.status, 404);
-    assert.strictEqual(response.data, "UAI, code formation et/ou millésime invalide");
-  });
-
-  it("should return a 404 if data exists but rates are empty", async () => {
-    await dbCollection("inserJeunesEtablissements").insertOne({
-      millesime: "2022-2021",
-      code_formation: "1022106",
-      type: "apprentissage",
-      diplome_renove_ou_nouveau: "non",
-      duree_de_formation: 1,
-      libelle_de_etablissement: "Centre de Formation",
-      libelle_de_la_formation: "cuisinier en desserts de restaurant",
-      region: {
-        code: "84",
-        nom: "Auvergne-Rhône-Alpes",
-      },
-      type_de_diplome: "MC5",
-      uai_de_etablissement: "0751234K",
+      assert.strictEqual(response.status, 200);
+      assert.ok(response.headers["content-type"].includes("image/svg+xml"));
+      assert.ok(response.data.includes(`width="320"`));
+      assert.ok(response.data.includes(`height="323"`));
+      assert.ok(response.data.includes("20%"));
+      assert.ok(response.data.includes("sont en emploi 6 mois"));
+      assert.ok(response.data.includes("poursuivent leurs études"));
     });
-    const { httpClient } = await startServer();
-    const response = await httpClient.get("/api/svg/uai/0751234K/code_formation/1022106/millesime/2022-2021");
-    assert.strictEqual(response.status, 404);
-    assert.strictEqual(response.data, "Donnée non disponible");
-  });
-
-  it("should return a 400 if uai is not well formated", async () => {
-    const { httpClient } = await startServer();
-    const response = await httpClient.get("/api/svg/uai/0010001/code_formation/23220023440/millesime/2020-2019");
-    assert.strictEqual(response.status, 400);
   });
 });
