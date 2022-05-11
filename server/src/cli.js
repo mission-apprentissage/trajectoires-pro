cfb2728ee5e1a674e34eeaac0790d0e9725153fa
Benoît Guérout <bguerout@gmail.com>
2022-05-11 20:42:12 +0200

    Mise à jour du cli

--- server/src/cli.js
+++ server/src/cli.js
@@ -8,6 +8,10 @@ const { createReadStream } = require("fs");
 const { promiseAllProps } = require("./common/utils/asyncUtils");
 const InserJeunes = require("./common/InserJeunes");
 
+function asArray(v) {
+  return v.split(",");
+}
+
 cli
   .command("importInserJeunesNationalData")
   .description("Importe les données InserJeunes nationales en base")
@@ -23,15 +27,16 @@ cli
 cli
   .command("importStats")
   .description("Importe les données statistiques de l'API InserJeunes")
-  .argument("[file]", "Un fichier CSV avec la liste des UAI dans une colonne ayant pour nom 'uai'", createReadStream)
-  .action((file) => {
+  .argument("<stats>", "Le nom des stats à importer (formations,certifications)", asArray)
+  .option("--file", "Un fichier CSV avec la liste des UAI dans une colonne ayant pour nom 'uai'", createReadStream)
+  .action((stats, { file }) => {
     runScript(async () => {
       const inserjeunes = new InserJeunes(); //Permet de partager le rate limiter entre les deux imports
       await inserjeunes.login();
 
       return promiseAllProps({
-        formations: importFormationsStats({ input: file, inserjeunes }),
-        certifications: importCertificationsStats({ inserjeunes }),
+        ...(stats.includes("formations") ? { formations: importFormationsStats({ input: file, inserjeunes }) } : {}),
+        ...(stats.includes("certifications") ? { certifications: importCertificationsStats({ inserjeunes }) } : {}),
       });
     });
   });
