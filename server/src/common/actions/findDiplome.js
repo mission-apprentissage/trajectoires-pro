33b2dc7f19a023e797be8163691bfa8cc8425a17
Benoît Guérout <bguerout@gmail.com>
2022-06-30 15:18:44 +0200

    Simplification des diplomes

--- server/src/common/actions/findDiplome.js
+++
@@ -1,7 +0,0 @@
-import { findCodeCertification } from "./findCodeCertification.js";
-
-export async function findDiplome(code) {
-  const certif = await findCodeCertification(code);
-
-  return certif?.diplome;
-}
