7ef79ede893faa6a9125b41749085aa3934a2c17
Benoît Guérout <bguerout@gmail.com>
2022-07-01 14:47:09 +0200

    Export des codes certifications uniques

--- server/src/jobs/exportCodeCertifications.js
+++ server/src/jobs/exportCodeCertifications.js
@@ -1,17 +1,27 @@
 import { oleoduc, transformData, transformIntoCSV } from "oleoduc";
 import { certificationsStats } from "../common/db/collections/collections.js";
 import { findCodeCertification } from "../common/actions/findCodeCertification.js";
-import { pick } from "lodash-es";
 import { dateAsString } from "../common/utils/stringUtils.js";
 
 export function exportCodeCertifications(output) {
   return oleoduc(
-    certificationsStats().find().stream(),
-    transformData(async (stats) => {
-      let code = await findCodeCertification(stats.code_certification);
+    certificationsStats()
+      .aggregate([
+        {
+          $group: {
+            _id: "$code_certification",
+            code_certification: { $first: "$code_certification" },
+            filiere: { $first: "$filiere" },
+          },
+        },
+      ])
+      .stream(),
+    transformData(async ({ code_certification, filiere }) => {
+      let code = await findCodeCertification(code_certification);
 
       return {
-        ...pick(stats, ["code_certification", "millesime", "filiere"]),
+        code_certification,
+        filiere,
         date_fermeture: dateAsString(code?.date_fermeture) || "",
       };
     }),
