f657a9499e52890b64c4c9fd00bf2f322c933c8a
Benoît Guérout <bguerout@gmail.com>
2022-07-08 11:33:15 +0200

    Suppresion des taux indisponibles

--- server/src/common/utils/mongodbUtils.js
+++ server/src/common/utils/mongodbUtils.js
@@ -1,23 +1,40 @@
 import { convertStats, getTauxReglesDeCalcul, TAUX, VALEURS } from "../stats/stats.js";
 
+function $(v) {
+  return `$${v}`;
+}
+
 export function $sumOf(field) {
   return { $sum: { $ifNull: [field, 0] } };
 }
 
 export function $percentage(dividend, divisor) {
   return {
-    $round: { $multiply: [{ $divide: [dividend, divisor] }, 100] },
+    $cond: [
+      { $or: [{ $eq: [divisor, 0] }, { $not: [divisor] }, { $not: [dividend] }] },
+      "$$REMOVE",
+      {
+        $round: {
+          $multiply: [
+            {
+              $divide: [dividend, divisor],
+            },
+            100,
+          ],
+        },
+      },
+    ],
   };
 }
 
 export function $valeursStats() {
-  return convertStats(VALEURS, (statName) => $sumOf(`$${statName}`));
+  return convertStats(VALEURS, (statName) => $sumOf($(statName)));
 }
 
 export function $tauxStats() {
   const regles = getTauxReglesDeCalcul();
   return convertStats(TAUX, (statName) => {
     const regle = regles[statName];
-    return regle ? $percentage(`$${regle.dividend}`, `$${regle.divisor}`) : null;
+    return regle ? $percentage($(regle.dividend), $(regle.divisor)) : null;
   });
 }
--- server/tests/jobs/computeRegionStats-test.js
+++ server/tests/jobs/computeRegionStats-test.js
@@ -4,6 +4,18 @@ import { insertFormationsStats } from "../utils/fakeData.js";
 import { formationsStats, regionStats } from "../../src/common/db/collections/collections.js";
 import { computeRegionStats } from "../../src/jobs/computeRegionStats.js";
 
+function insertNoStats() {
+  return formationsStats().insertOne({
+    uai: "0751234J",
+    millesime: "2018_2019",
+    filiere: "apprentissage",
+    code_certification: "12345678",
+    code_formation_diplome: "12345678",
+    diplome: { code: "4", libelle: "BAC" },
+    region: { code: "11", nom: "Île-de-France" },
+  });
+}
+
 describe("computeRegionStats", () => {
   it("Vérifie qu'on peut calculer les stats d'une région", async () => {
     await Promise.all([
@@ -37,9 +49,9 @@ describe("computeRegionStats", () => {
       }),
     ]);
 
-    let stats = await computeRegionStats();
+    const stats = await computeRegionStats();
 
-    let found = await regionStats().findOne({}, { projection: { _id: 0 } });
+    const found = await regionStats().findOne({}, { projection: { _id: 0 } });
     assert.deepStrictEqual(omit(found, ["_meta"]), {
       region: {
         code: "11",
@@ -72,15 +84,7 @@ describe("computeRegionStats", () => {
 
   it("Vérifie qu'on peut calculer les stats d'une région et qu'on ignore les données absentes", async () => {
     await Promise.all([
-      formationsStats().insertOne({
-        uai: "0751234J",
-        millesime: "2018_2019",
-        filiere: "apprentissage",
-        code_certification: "12345678",
-        code_formation_diplome: "12345678",
-        diplome: { code: "4", libelle: "BAC" },
-        region: { code: "11", nom: "Île-de-France" },
-      }),
+      insertNoStats(),
       insertFormationsStats({
         filiere: "apprentissage",
         code_certification: "12345678",
@@ -97,9 +101,9 @@ describe("computeRegionStats", () => {
       }),
     ]);
 
-    let stats = await computeRegionStats();
+    await computeRegionStats();
 
-    let found = await regionStats().findOne({}, { projection: { _id: 0 } });
+    const found = await regionStats().findOne({}, { projection: { _id: 0 } });
     assert.deepStrictEqual(omit(found, ["_meta"]), {
       region: {
         code: "11",
@@ -126,17 +130,10 @@ describe("computeRegionStats", () => {
       taux_emploi_6_mois: 20,
       taux_poursuite_etudes: 10,
     });
-    assert.ok(found._meta.date_import);
-    assert.deepStrictEqual(stats, { created: 1, failed: 0, updated: 0 });
   });
 
   it("Vérifie qu'on peut mettre à jour les données d'une stats de région", async () => {
     await insertFormationsStats({
-      region: { code: "11", nom: "Île-de-France" },
-      filiere: "apprentissage",
-      code_certification: "12345678",
-      code_formation_diplome: "12345678",
-      diplome: { code: "4", libelle: "BAC" },
       nb_annee_term: 200,
       nb_poursuite_etudes: 20,
       taux_poursuite_etudes: 10,
@@ -150,12 +147,48 @@ describe("computeRegionStats", () => {
         },
       }
     );
-    let stats = await computeRegionStats();
 
-    let found = await regionStats().findOne({}, { projection: { _id: 0 } });
+    await computeRegionStats();
+
+    const found = await regionStats().findOne({}, { projection: { _id: 0 } });
     assert.deepStrictEqual(found.nb_annee_term, 300);
     assert.deepStrictEqual(found.taux_poursuite_etudes, 7);
-    assert.ok(found._meta.date_import);
-    assert.deepStrictEqual(stats, { created: 0, failed: 0, updated: 1 });
+  });
+
+  it("Vérifie qu'on ignore les taux avec des stats à 0", async () => {
+    await Promise.all([
+      insertFormationsStats({
+        nb_sortant: 0,
+        nb_annee_term: 0,
+      }),
+    ]);
+
+    await computeRegionStats();
+
+    let found = await regionStats().findOne({}, { projection: { _id: 0 } });
+    assert.strictEqual(found.nb_sortant, 0);
+    assert.strictEqual(found.taux_emploi_24_mois, undefined);
+    assert.strictEqual(found.taux_emploi_18_mois, undefined);
+    assert.strictEqual(found.taux_emploi_12_mois, undefined);
+    assert.strictEqual(found.taux_emploi_6_mois, undefined);
+  });
+
+  it("Vérifie qu'on ignore les taux avec des stats indisponibles", async () => {
+    await Promise.all([
+      insertNoStats(),
+      insertFormationsStats({
+        nb_sortant: 0,
+        nb_annee_term: 0,
+      }),
+    ]);
+
+    await computeRegionStats();
+
+    let found = await regionStats().findOne({}, { projection: { _id: 0 } });
+    assert.strictEqual(found.nb_sortant, 0);
+    assert.strictEqual(found.taux_emploi_24_mois, undefined);
+    assert.strictEqual(found.taux_emploi_18_mois, undefined);
+    assert.strictEqual(found.taux_emploi_12_mois, undefined);
+    assert.strictEqual(found.taux_emploi_6_mois, undefined);
   });
 });
