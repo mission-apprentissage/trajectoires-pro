a2843f25e406ea8c3ed6d3493a2f2a2270ef6ae7
Benoit Guérout <bguerout@gmail.com>
2022-09-23 14:27:38 +0200

    Simplification du script de migration (#122)

--- server/src/jobs/migrate.js
+++ server/src/jobs/migrate.js
@@ -1,36 +1,18 @@
-import { configureIndexes, configureValidation, dbCollection } from "../common/db/mongodb.js";
+import { dbCollection, migrateMongodb } from "../common/db/mongodb.js";
 import { promiseAllProps } from "../common/utils/asyncUtils.js";
 
 const VERSION = 1;
 
-async function tasks() {
-  return promiseAllProps({
-    codeFormationDiplomes: dbCollection("codeFormationDiplomes").drop(),
-    certificationsStats: dbCollection("certificationsStats").drop(),
-    formationsStats: dbCollection("formationsStats").drop(),
-  });
-}
-
-async function _ensureMigrationCanBeRun() {
-  let count = await dbCollection("migrations").count({ version: VERSION });
-  if (count > 0) {
-    throw new Error(`La migration ${VERSION} a déjà été réalisée`);
-  }
-}
-
-async function _prepareMigration(options) {
-  await configureIndexes({ dropIndexes: options.dropIndexes || false });
-  await configureValidation();
-}
-
-function _saveMigration() {
-  return dbCollection("migrations").insertOne({ version: VERSION });
-}
-
 export async function migrate(options = {}) {
-  await _ensureMigrationCanBeRun();
-  await _prepareMigration(options);
-  let res = await tasks();
-  await _saveMigration();
-  return res;
+  return migrateMongodb(
+    VERSION,
+    () => {
+      return promiseAllProps({
+        codeFormationDiplomes: dbCollection("codeFormationDiplomes").drop(),
+        certificationsStats: dbCollection("certificationsStats").drop(),
+        formationsStats: dbCollection("formationsStats").drop(),
+      });
+    },
+    options
+  );
 }
