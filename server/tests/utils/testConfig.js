03cc009d216031d087b3739c1d1534c1c998e2de
Benoit Guérout <bguerout@gmail.com>
2022-10-10 10:32:42 +0200

    Renommage de la varibale d'env pour l'api key (#137)

--- server/tests/utils/testConfig.js
+++ server/tests/utils/testConfig.js
@@ -2,4 +2,4 @@ import { v4 } from "uuid";
 process.env.TRAJECTOIRES_PRO_OVH_STORAGE_URI = "https://ovh.com";
 process.env.TRAJECTOIRES_PRO_INSERJEUNES_USERNAME = "test";
 process.env.TRAJECTOIRES_PRO_INSERJEUNES_PASSWORD = v4();
-process.env.TRAJECTOIRES_PRO_INSERJEUNES_API_KEY = v4();
+process.env.TRAJECTOIRES_PRO_API_KEY = v4();
