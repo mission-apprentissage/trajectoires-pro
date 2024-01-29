a1570a0c1d7ddef48533a88e7d557cffb3496ab1
Ananda <ananda10@gmail.com>
2024-01-29 16:34:35 +0100

    chore: suppression de la récupération des millésimes 2018_2019 et 2019

--- server/src/config.js
+++ server/src/config.js
@@ -71,11 +71,8 @@ const config = {
     },
   },
   millesimes: {
-    default: env.get("TRAJECTOIRES_PRO_MILLESIMES").default("2019,2020,2021,2022").asArray(),
-    formations: env
-      .get("TRAJECTOIRES_PRO_MILLESIMES_FORMATIONS")
-      .default("2018_2019,2019_2020,2020_2021,2021_2022")
-      .asArray(),
+    default: env.get("TRAJECTOIRES_PRO_MILLESIMES").default("2020,2021,2022").asArray(),
+    formations: env.get("TRAJECTOIRES_PRO_MILLESIMES_FORMATIONS").default("2019_2020,2020_2021,2021_2022").asArray(),
     regionales: env.get("TRAJECTOIRES_PRO_MILLESIMES_REGIONALES").default("2019_2020,2020_2021,2021_2022").asArray(),
   },
 };
