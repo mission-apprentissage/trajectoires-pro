caf2e1acdd2fd89662114b2ba6ade51385764371
Ananda <ananda10@gmail.com>
2024-02-05 14:29:06 +0100

    fix: missing metabase id

--- ui/app/(statistiques)/statistiques/config.ts
+++ ui/app/(statistiques)/statistiques/config.ts
@@ -2,7 +2,7 @@ const Config = {
   METABASE: {
     dashboards: {
       stats: {
-        id: parseInt(process.env.METABASE_DASHBOARD_STATS_ID || ""), //53,
+        id: parseInt(process.env.METABASE_DASHBOARD_STATS_ID || "53"), //53,
         defaultQueryParams: {
           adresse_ip: ["213.91.3.170", "213.91.3.180", "195.83.117.34"],
           consommateur: "ONISEP",
@@ -10,11 +10,11 @@ const Config = {
         hideParams: ["adresse_ip", "consommateur"],
       },
       statsDetails: {
-        id: parseInt(process.env.METABASE_DASHBOARD_STATS_DETAILS_ID || ""), //47,
+        id: parseInt(process.env.METABASE_DASHBOARD_STATS_DETAILS_ID || "47"), //47,
         hideParams: ["adresse_ip"],
       },
       statsRegion: {
-        id: parseInt(process.env.METABASE_DASHBOARD_STATS_REGION_ID || ""), // 58
+        id: parseInt(process.env.METABASE_DASHBOARD_STATS_REGION_ID || "58"), // 58
       },
     },
     metrics: {
