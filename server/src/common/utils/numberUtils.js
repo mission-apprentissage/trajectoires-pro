9620d637c045a0352830edd37cc0cad49ba69853
Ananda <ananda10@gmail.com>
2024-02-20 12:03:15 +0100

    fix: arrondi les pourcentage en utilisant le banker's rounding

--- server/src/common/utils/numberUtils.js
+++ server/src/common/utils/numberUtils.js
@@ -1,6 +1,8 @@
+import roundHalfEven from "round-half-even";
+
 export function percentage(dividend, divisor) {
   // Round like mongodb, when X.5 round to the nearest even
   // Avoid having rate > 100 when additioning 2 percentages rounded
-  const percent = Math.round((dividend / divisor) * 100);
-  return percent - (percent % 2);
+  const percent = roundHalfEven((dividend / divisor) * 100, 0);
+  return percent;
 }
