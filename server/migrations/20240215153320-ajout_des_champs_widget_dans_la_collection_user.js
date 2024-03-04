c6936ba8bb2491b828bfb5ef805b38c01101388e
Ananda <ananda10@gmail.com>
2024-03-04 16:54:38 +0100

    fix: champs widget dans la migration

--- server/migrations/20240215153320-ajout_des_champs_widget_dans_la_collection_user.js
+++ server/migrations/20240215153320-ajout_des_champs_widget_dans_la_collection_user.js
@@ -1,5 +1,6 @@
 import { string, arrayOf, object, number } from "#src/common/db/collections/jsonSchema/jsonSchemaTypes.js";
 import * as MongoDB from "#src/common/db/mongodb.js";
+import uniqid from "uniqid";
 
 const schema = {
   properties: {
@@ -20,6 +21,25 @@ const schema = {
 export const up = async (db, client) => {
   MongoDB.setMongoDBClient(client);
   await MongoDB.mergeSchema("users", schema);
+
+  const users = await MongoDB.dbCollection("users").find({}).toArray();
+  for (const user of users) {
+    if (!user?.widget) {
+      await MongoDB.dbCollection("users").updateOne(
+        {
+          _id: user._id,
+        },
+        {
+          $set: {
+            widget: {
+              hash: uniqid(),
+              version: [],
+            },
+          },
+        }
+      );
+    }
+  }
 };
 
 export const down = async (db, client) => {
