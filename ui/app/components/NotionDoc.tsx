9f9b0a6490887812071099a2b4dea7e104a4991a
Ananda <ananda10@gmail.com>
2024-04-03 12:30:46 +0200

    fix: affichage des blocs de codes dans Notion (#236)

--- ui/app/components/NotionDoc.tsx
+++ ui/app/components/NotionDoc.tsx
@@ -2,6 +2,11 @@
 import { ExtendedRecordMap } from "notion-types";
 import { Suspense } from "react";
 import { NotionRenderer } from "react-notion-x";
+import dynamic from "next/dynamic";
+import "prismjs/themes/prism-tomorrow.css";
+import "./NotionDoc.css";
+
+const Code = dynamic(() => import("react-notion-x/build/third-party/code").then((m) => m.Code));
 
 const NotionDoc = ({
   urlBasePath,
@@ -29,6 +34,9 @@ const NotionDoc = ({
         recordMap={recordMap}
         fullPage={true}
         darkMode={false}
+        components={{
+          Code,
+        }}
       />
     </Suspense>
   );
