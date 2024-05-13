dac58d77a86b0243f7ece41eb56eba7c20065abd
Ananda <ananda10@gmail.com>
2024-05-13 15:42:11 +0200

    feat: change date transport

--- server/src/http/routes/formationsRoutes.js
+++ server/src/http/routes/formationsRoutes.js
@@ -310,6 +310,7 @@ export default () => {
             point: `${latitude},${longitude}`,
             departureTime: moment().set("hour", 8).toDate(),
             buckets: buckets.filter((b) => b <= timeLimit),
+            reverse_flow: true,
           });
 
           filtersEtablissement.push(getTimeFilter({ coordinate: { longitude, latitude }, isochroneBuckets }));
