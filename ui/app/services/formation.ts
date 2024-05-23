58abc97950df5e92df0c5b7ac261cc6cae5e95ce
Ananda <ananda10@gmail.com>
2024-05-23 11:30:41 +0200

    style: changement des couleurs des labels

--- ui/app/services/formation.ts
+++ ui/app/services/formation.ts
@@ -6,14 +6,14 @@ export const FORMATION_TAG: { tag: FormationTag; libelle: string; color: string;
     tag: FormationTag.POUR_TRAVAILLER_RAPIDEMENT,
     libelle: "POUR TRAVAILLER RAPIDEMENT",
     color: "#18753c",
-    bgColor: "",
+    bgColor: "var(--success-975-75)",
     icon: "fr-icon-shopping-cart-2-fill",
   },
   {
     tag: FormationTag.POUR_CONTINUER_DES_ETUDES,
     libelle: "POUR CONTINUER DES ÉTUDES",
-    color: "#b34000",
-    bgColor: "#FFE9E9",
+    color: "var(--info-425-625)",
+    bgColor: "var(--info-975-75)",
     icon: "fr-icon-book-2-fill",
   },
 ];
