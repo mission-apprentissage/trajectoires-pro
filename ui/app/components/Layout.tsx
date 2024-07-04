a09252c809b8721ba804f02879470fb6204139d9
Ananda <ananda10@gmail.com>
2024-07-04 15:13:06 +0200

    fix(prescripteur): test fix layout

--- ui/app/components/Layout.tsx
+++ ui/app/components/Layout.tsx
@@ -1,8 +1,10 @@
+"use client";
 import { AppRouterCacheProvider } from "@mui/material-nextjs/v14-appRouter";
 import { DsfrProvider } from "@codegouvfr/react-dsfr/next-appdir/DsfrProvider";
 import MuiDsfrThemeProvider from "#/app/components/MuiDsfrThemeProvider";
 import { Header } from "@codegouvfr/react-dsfr/Header";
 import Providers from "#/app/provider";
+import { StyledEngineProvider } from "@mui/material/styles";
 
 export default function Layout({
   title,
@@ -18,40 +20,42 @@ export default function Layout({
   return (
     <DsfrProvider>
       <AppRouterCacheProvider options={{ key: "css", speedy: true }}>
-        <MuiDsfrThemeProvider>
-          <Providers>
-            {header || (
-              <Header
-                brandTop={
-                  <>
-                    RÉPUBLIQUE
-                    <br />
-                    FRANÇAISE
-                  </>
-                }
-                serviceTitle={title}
-                serviceTagline={tagline}
-                homeLinkProps={{
-                  href: "/",
-                  title,
+        <StyledEngineProvider injectFirst>
+          <MuiDsfrThemeProvider>
+            <Providers>
+              {header || (
+                <Header
+                  brandTop={
+                    <>
+                      RÉPUBLIQUE
+                      <br />
+                      FRANÇAISE
+                    </>
+                  }
+                  serviceTitle={title}
+                  serviceTagline={tagline}
+                  homeLinkProps={{
+                    href: "/",
+                    title,
+                  }}
+                  quickAccessItems={[]}
+                />
+              )}
+              <div
+                style={{
+                  flex: 1,
+                  display: "flex",
+                  flexDirection: "column",
+                  width: "100%",
+                  margin: "auto",
+                  backgroundColor: "#fff",
                 }}
-                quickAccessItems={[]}
-              />
-            )}
-            <div
-              style={{
-                flex: 1,
-                display: "flex",
-                flexDirection: "column",
-                width: "100%",
-                margin: "auto",
-                backgroundColor: "#fff",
-              }}
-            >
-              {children}
-            </div>
-          </Providers>
-        </MuiDsfrThemeProvider>
+              >
+                {children}
+              </div>
+            </Providers>
+          </MuiDsfrThemeProvider>
+        </StyledEngineProvider>
       </AppRouterCacheProvider>
     </DsfrProvider>
   );
