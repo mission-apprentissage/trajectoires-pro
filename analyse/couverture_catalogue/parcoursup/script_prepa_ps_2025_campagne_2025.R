64253af2ba930d5c835e3c93031b8e399fc17bb5
Arnaud Milet <arnaud.milet@d-sidd.com>
2025-02-20 17:57:13 +0100

    mofification du script d'appel au script ps

--- analyse/couverture_catalogue/parcoursup/script_prepa_ps_2025_campagne_2025.R
+++ analyse/couverture_catalogue/parcoursup/script_prepa_ps_2025_campagne_2025.R
@@ -4,7 +4,9 @@ library(readxl)
 
 setwd(dirname(rstudioapi::getSourceEditorContext()$path))
 
-chemin_projet <- "C:/Users/arnau/d-sidd Dropbox/Arnaud milet/0_beta/1- Exposition/Groupe-002 - Parcoursup/002 - 4 - Prepa ParcourSup 2025/2_script_appariement_ps_2025"
+
+chemin_projet_generique <- "C:/Users/arnau/d-sidd Dropbox/Arnaud milet/0_beta/1- Exposition/Groupe-002 - Parcoursup/002 - 4 - Prepa ParcourSup 2025/script_appariement_ps"
+chemin_projet_2025 <- "C:/Users/arnau/d-sidd Dropbox/Arnaud milet/0_beta/1- Exposition/Groupe-002 - Parcoursup/002 - 4 - Prepa ParcourSup 2025/2_script_appariement_ps_2025"
 # chemin_projet <- file.path("../../../../Groupe-002 - Parcoursup/002 - 4 - Prepa ParcourSup 2025/2_script_appariement_ps_2025")
 
 #lancer les scripts d'appariement ----
@@ -14,13 +16,30 @@ chemin_projet <- "C:/Users/arnau/d-sidd Dropbox/Arnaud milet/0_beta/1- Expositio
 
 # source("2_script_appariement_ps_2025/3_0_script_prepa_ps_2025_import_data.R")
 # rm(list=ls())
-load(file.path(chemin_projet,"prepa_ps_2025.RData"))
+load(file.path(chemin_projet_2025,"prepa_ps_2025.RData"))
 
-source(file.path(chemin_projet,"3_1_script_prepa_ps_2025_prepa_data.R"))
-source(file.path(chemin_projet,"3_2_script_prepa_ps_2025_focus_ij.R"))
-source(file.path(chemin_projet,"3_3_script_prepa_ps_2025_focus_sup.R"))
-source(file.path(chemin_projet,"3_4_script_prepa_ps_2025_ajout_remuneration.R"))
-source(file.path(chemin_projet,"3_5_script_prepa_ps_2025_raison_non_couverture_non_ij_non_sup.R"))
-source(file.path(chemin_projet,"3_6_script_prepa_ps_2025_appariement_sies_passage_reussite.R"))
-source(file.path(chemin_projet,"3_7_script_prepa_ps_2025_debug_continuum.R"))
-source(file.path(chemin_projet,"3_8_script_prepa_ps_2025_analyse_couverture.R"))
+chemin_racine_data <- "../../../../0- data" 
+chemin_racine_data <- "../../../../Groupe-002 - Parcoursup/002 - 4 - Prepa ParcourSup 2025/script_appariement_ps/data_utiles"
+chemin_racine_data <- "C:/Users/arnau/d-sidd Dropbox/Arnaud milet/0_beta/1- Exposition/Groupe-002 - Parcoursup/002 - 4 - Prepa ParcourSup 2025/script_appariement_ps/data_utiles"
+source(file.path(chemin_projet_2025,"3_0_script_prepa_ps_2025_patch_insersup_api.R"))
+print("Fin patch_insersup_api")
+source(file.path(chemin_projet_generique,"scripts_appariements/1_script_prepa_ps_prepa_data.R"))
+print("Fin 1_script_prepa_ps_prepa_data.R")
+
+source(file.path(chemin_projet_generique,"scripts_appariements/2_script_prepa_ps_focus_ij.R"))
+print("Fin 2_script_prepa_ps_focus_ij.R")
+
+source(file.path(chemin_projet_generique,"scripts_appariements/3_script_prepa_ps_focus_sup.R"))
+print("Fin 3_script_prepa_ps_focus_sup.R")
+
+source(file.path(chemin_projet_generique,"scripts_appariements/4_script_prepa_ps_ajout_remuneration.R"))
+print("Fin 4_script_prepa_ps_ajout_remuneration.R")
+
+source(file.path(chemin_projet_generique,"scripts_appariements/5_script_prepa_ps_raison_non_couverture_non_ij_non_sup.R"))
+print("Fin 5_script_prepa_ps_raison_non_couverture_non_ij_non_sup.R")
+
+source(file.path(chemin_projet_generique,"scripts_appariements/6_script_prepa_ps_appariement_sies_passage_reussite.R"))
+print("Fin 6_script_prepa_ps_appariement_sies_passage_reussite.R")
+
+source(file.path(chemin_projet_generique,"scripts_appariements/7_script_prepa_ps_analyse_couverture.R"))
+print("Fin 7_script_prepa_ps_analyse_couverture.R")
